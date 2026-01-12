70f079a92fea5ba4e9b703d7a3ab4ee6f65e942d
Michał Dzierzbicki <michal.dzierzbicki@vp.pl>
2026-01-12 07:32:42 +0100

    Fix ecosystem to work on server

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,16 +1,31 @@
+// module.exports = {
+//   apps : [{
+//     env: {
+//       NODE_ENV: "development",
+//       PORT: "3000"
+//     },
+//     name: "next-app",
+//     script: "npm",
+//     args: "start",
+//     cwd: "/var/www/html/bear2eat/frontend/",
+//     watch: true,
+//     env: {
+//       NODE_ENV: "production",
+//     }
+//   }]
+// };
+
 module.exports = {
-  apps : [{
-    env: {
-      NODE_ENV: "development",
-      PORT: "3000"
-    },
-    name: "next-app",
-    script: "npm",
-    args: "start",
-    cwd: "/var/www/html/bear2eat/frontend/",
-    watch: true,
-    env: {
-      NODE_ENV: "production",
+  apps: [
+    {
+      name: "next-app",
+      cwd: "/var/www/html/bear2eat/frontend",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      env: {
+        NODE_ENV: "development",
+      },
+      watch: true
     }
-  }]
-};
+  ]
+}
